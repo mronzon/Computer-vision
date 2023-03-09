541bcf21e3f6b6c196bddc3972d151b2b11c6406
mronzon <104356392+mronzon@users.noreply.github.com>
2023-03-09 11:38:33 -0500

    Make the SLIC method work

--- Code/TP2/main.py
+++ Code/TP2/main.py
@@ -3,14 +3,41 @@ import os
 import cv2
 from skimage import img_as_float, img_as_ubyte
 from skimage.io import imsave
-
+import numpy as np
 from os import walk
 
-from skimage.segmentation import slic, mark_boundaries
+from skimage.segmentation import slic, mark_boundaries, find_boundaries
 
 parser = argparse.ArgumentParser(description="Récupération des éléments presents sur un sol.")
 parser.add_argument("--folder", default="", type=str, help="Le chemin vers le dossier contenant les images.")
 args = parser.parse_args()
+
+
+def crop_image(img, label):
+    result = img.copy()
+    min_y = len(label)
+    min_x = len(label)
+    max_x = -1
+    max_y = -1
+    for y in range(len(label)):
+        for x in range(len(label[y])):
+            if label[y][x] != 0:
+                min_x = min(x, min_x)
+                min_y = min(y, min_y)
+                max_x = max(x, max_x)
+                max_y = max(y, max_y)
+    return result[min_y:max_y, min_x:max_x]
+
+
+def black_proportion(image):
+    total = 0
+    black = 0
+    for y in image:
+        for x in y:
+            total += 1
+            if x.all() == 0:
+                black += 1
+    return black / total
 
 
 def equalize_luminosity(img):
@@ -44,5 +71,19 @@ if __name__ == "__main__":
 
         segments_slic = slic(img, n_segments=100, compactness=10, sigma=1)
 
-        imsave(result_folder + "\\" + os.path.splitext(filename)[0] + "_gray_equalized" + os.path.splitext(filename)[1], thresh_gray_equalized)  # Save the image
-        imsave(result_folder + "\\" + os.path.splitext(filename)[0] + "_slic" + os.path.splitext(filename)[1], img_as_ubyte(mark_boundaries(img, segments_slic)))  # Save the image
+        if not os.path.exists(result_folder + "\\" + os.path.splitext(filename)[0]):
+            os.mkdir(result_folder + "\\" + os.path.splitext(filename)[0])
+
+        unique_label = np.unique(segments_slic)
+        i = 0
+        for label in unique_label:
+            segment_label = segments_slic.copy()
+            segment_label[segment_label != label] = 0
+            image_mask = image[thresh_gray_equalized == 0] = 0
+            result = crop_image(image, segment_label)
+            if black_proportion(result) > 0.5:
+                continue
+
+            imsave(result_folder + "\\" + os.path.splitext(filename)[0] + "\\rock_" + str(i) + os.path.splitext(filename)[1], img_as_ubyte(result))  # Save the image
+            i += 1
+
